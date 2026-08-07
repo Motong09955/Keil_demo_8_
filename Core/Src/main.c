c6d43dac8e7ca38fc3b443cc9d4c3b00a356aa27
motong <motong09955@outlook.com>
2026-08-07 08:58:25 +0800

    start_form_LED2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -51,7 +51,7 @@
 /* #define 是宏定义：编译前会把名字替换成后面的内容 */
 #define LED_COUNT   4U    /* 板载 LED 数量，U 表示 unsigned 无符号数 */
 #define BLINK_TIMES 5U    /* 每颗 LED 闪烁次数 */
-#define DELAY_MS    500U  /* LED 亮/灭持续时间，单位毫秒 */
+#define DELAY_MS    250U  /* LED 亮/灭持续时间，单位毫秒 */
 #define BEEP_MS     50U  /* 蜂鸣器响一声的时长 */
 /* USER CODE END PD */
 
@@ -89,7 +89,7 @@ int main(void)
 
   /* USER CODE BEGIN 1 */
   /* 局部变量：只能在 main 函数内使用 */
-  uint8_t  current_led = 2U;              /* 当前要操作的 LED 编号 */
+  uint8_t  current_led = 1U;              /* 当前要操作的 LED 编号 */
   uint16_t blink_times = BLINK_TIMES;/* 每颗 LED 闪烁次数 */
   uint32_t delay_ms    = DELAY_MS;   /* 亮/灭延时 */
   const uint8_t led_count = LED_COUNT; /* const 表示该变量不允许修改 */
@@ -128,7 +128,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    current_led = 1U; /* 练习点：改成 2U，观察从哪颗 LED 开始 */
+    current_led = 2U; /* 练习点：改成 2U，观察从哪颗 LED 开始 */
 
     /* while 循环：条件成立就反复执行 {} 里的代码 */
     while (current_led <= led_count)
